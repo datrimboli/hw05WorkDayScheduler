20f36f8cc8ff7e43d2788013c3c013d055831096
Dave Trimboli <davidatrimboli@gmail.com>
2020-07-30 15:16:30 -0700

    past,present,future

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 // Date display
 $('#currentDay').html(moment().format('dddd, MMMM Do, YYYY'));
 
+const currentTime = moment().format('h');
+console.log(currentTime)
+
+let hours = [9, 10, 11, 12, 1, 2, 3, 4, 5]
+let currentIndex = hours.indexOf(parseInt(currentTime))
+
+
+// Determines if times are in the past, present, or future
+for (let i = 0; i < hours.length; i++) {
+  if (i < currentIndex) {
+    $(`.hour-${hours[i]}`).addClass('past')
+  } else if (i > currentIndex) {
+    $(`.hour-${hours[i]}`).addClass('future')
+  } else {
+    $(`.hour-${hours[i]}`).addClass('present')
+  }
+}
