70dcb263e7489678b09180dc998b3626c2af2b78
Dave Trimboli <davidatrimboli@gmail.com>
2020-07-30 18:13:02 -0700

    JS local storage

--- app.js
+++ app.js
@@ -20,3 +20,20 @@ for (let i = 0; i < hours.length; i++) {
     $(`.hour-${hours[i]}`).addClass('present')
   }
 }
+
+$(".saveBtn").click(function () {
+  var userInput = $(this).siblings('textarea').val()
+  console.log($(this).siblings('textarea').attr('id'))
+  var hour = $(this).siblings('textarea').attr('id')
+  localStorage.setItem(hour, userInput)
+
+})
+$('#9').val(localStorage.getItem('9'))
+$('#10').val(localStorage.getItem('10'))
+$('#11').val(localStorage.getItem('11'))
+$('#12').val(localStorage.getItem('12'))
+$('#1').val(localStorage.getItem('1'))
+$('#2').val(localStorage.getItem('2'))
+$('#3').val(localStorage.getItem('3'))
+$('#4').val(localStorage.getItem('4'))
+$('#5').val(localStorage.getItem('5'))
